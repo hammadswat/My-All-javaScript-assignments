3ad20f8df5e6d539b3fd8ba0938d2ab1f7170d13
HAMMAD SWATI <145581913+hammadswati777@users.noreply.github.com>
2024-01-28 23:53:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+    //js assignment 01 //
+
+//** VARIABLES	FOR	STRINGS **//
+
+//.......................................................................//
+
+
+
+// 01) Declare a variable called username.
+ var username 
+
+
+
+// 02) Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Hammad Swati";
+
+
+
+// 03) Write a script to 
+//a. declare a JS variable, titled message
+//b. assign “Hello World” to the variable message
+//c. display the message in alert box
+var message = "Hello World";
+alert(message);
+
+
+
+
+
+// 04) Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+var student   = "Hammad swati";
+alert(student);
+
+var age       = "21 years old";
+alert(age);
+
+var institute = "SSUET";
+alert(institute);
+
+var Module    =  "Mobile & cloud Computing-Module A";
+alert(Module);
+
+
+
+
+// 5)  Write a script to display the following alert using one JS variable:
+var food = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(food);
+
+
+
+
+// 06) Fortune Teller: Why pay a fortuneteller when you can just program your fortune yourself?
+//Store the following into variables: number of children, partner’s name, geographic location, and job title.
+var childrenNum = "2 kids"
+var partner     = "Monica Bellucci"
+var country     = "ABC,"
+var job         = "Software Developer"
+alert("You will be a " +job+ " in "+country + " Country and marrid to " + partner+ " with "+childrenNum);
+
+
+
+
+// 07)  Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com). Show 
+//the below mentioned message in an alert box. (Hint: use string concatenation).
+var email = "hammadswati777@gmail.com";
+alert("My email address is " + email);
+
+
+
+
+
+// 08) Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the followingmessage in an alert box:
+var book = "“A smarter way to learn JavaScript” "
+alert("I am trying to learn from the book "+book);
+
+
+
+
+// 9. Use the variables, declared in exercise 4 & show the details giving meaning to data. Like “My name is Ali Khan”, “I am 17 years old”, “I study in SSUET”, “I take Mobile & Cloud 
+//Computing, Module A classes”.
+var student   = "Hammad swati.";
+var age       = "21 years old.";
+var institute = "SSUET.";
+var Module    =  "Mobile & cloud Computing-Module A classes.";
+alert("My name is "+student +" I am "+age+ " I study in "+ institute+ " I take "+ Module);
+
+
+
+
+// 10) Write a script to display this in browser through JS.
+var content = "Yay! I can write HTML content through Javascript."
+document.write(content);
+
+
+
+//================> *** THE END *** <===============//
+
+
+
+
+
